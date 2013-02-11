684910a710631f238a40811ecdef45b1aed8969a
Jakob Voß <jakob.voss@gbv.de>
2013-02-11 11:27:36 +0000

    Import of VOJ/Config-Any-CSV-0.02 from CPAN.
    gitpan-cpan-distribution: Config-Any-CSV
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         VOJ/Config-Any-CSV-0.02.tar.gz
    gitpan-cpan-author:       VOJ
    gitpan-cpan-maturity:     released

--- lib/Config/Any/CSV.pm
+++ lib/Config/Any/CSV.pm
@@ -1,8 +1,8 @@
 package Config::Any::CSV;
 {
-  $Config::Any::CSV::VERSION = '0.01';
+  $Config::Any::CSV::VERSION = '0.02';
 }
-#ABSTRACT: Load CSV files as config files
+#ABSTRACT: Load CSV as config files
 
 use strict;
 use warnings;
@@ -13,19 +13,25 @@ use base 'Config::Any::Base';
 use Text::CSV;
 
 sub load {
-    my ($class, $file) = @_;
+    my ($class, $file, $driver) = @_;
 
-    my $csv = Text::CSV->new( { binary => 1, allow_whitespace => 1 } );
+    my $args = { binary => 1, allow_whitespace => 1 };
+    if ($driver) {
+        $args->{$_} = $driver->{$_} for keys %$driver;
+    }
+    my $csv = Text::CSV->new( $args );
     my $config = { };
     open my $fh, "<", $file or die $!;
 
+    my $default = $args->{empty_is_undef} ? undef : "";
+ 
     my $names = $csv->getline($fh);
     if ( $names ) {
         my $columns = scalar @$names - 1;
         while ( my $row = $csv->getline( $fh ) ) {
             next if @$row == 1 and $row->[0] eq ''; # empty line
             $config->{ $row->[0] // "" } = {
-                map { ( $names->[$_] // "" ) => $row->[$_] }
+                map { ( $names->[$_] // "" ) => ( $row->[$_] // $default ) }
                 (1..$columns)
             };
         }
@@ -48,20 +54,28 @@ __END__
 
 =head1 NAME
 
-Config::Any::CSV - Load CSV files as config files
+Config::Any::CSV - Load CSV as config files
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 SYNOPSIS
 
-The interface of Config::Any sucks, so this example uses Config::JFDI:
+    use Config::Any;
+ 
+    my $config = Config::Any->load_files({files => \@files});
 
-    use Config::JFDI;
+I recommend to use L<Config::ZOMG>:
 
-    my $config = Config::JFDI->new( 
-        path => '/path/that/includes/foo.csv', # and/or foo_local.csv
+    use Config::ZOMG;
+
+    # just load a single file
+    my $config_hash = Config::ZOMG->open( $csv_file );
+
+    # load foo.csv (and possible foo_local.csv)
+    my $config = Config::ZOMG->new( 
+        path => '/path/to/config'
         name => 'foo'
     );
 
@@ -69,10 +83,11 @@ The interface of Config::Any sucks, so this example uses Config::JFDI:
 
 This small module adds support of CSV files to L<Config::Any>. Files with
 extension C<.csv> are read with L<Text::CSV> - see that module for
-documentation of the particular CSV format. Config::Any::CSV enables the option
-C<binary> and C<allow_whitespace>. The first row of a CSV file is always
-interpreted as a list of field names and the first field is always interpreted
-as key field. For instance this CSV file
+documentation of the particular CSV format. By default, Config::Any::CSV
+enables the options C<binary> and C<allow_whitespace>.  One can modify options
+with C<driver_args> (L<Config::Any>) or C<driver> (L<Config::ZOMG>). The first
+row of a CSV file is always interpreted as a list of field names and the first
+field is always interpreted as key field. For instance this CSV file
 
     name,age,mail
     alice, 42, alice@example.org
@@ -83,7 +98,7 @@ Is parsed into this Perl structure:
     {
         alice => {
             age  => '42',
-            mail => 'alice@example.org'
+            mail => 'alice@example.org',
         },
          bob => {
             age => '23',
@@ -91,13 +106,12 @@ Is parsed into this Perl structure:
         }
     }
 
-The order of rows gets lost. If a file contains multiple rows with the same
-first field value, only the last of these rows is used. Empty lines are
-ignored.
+The name of the first field is irrelevant and the order of rows gets lost. If a
+file contains multiple rows with the same first field value, only the last of
+these rows is used. Empty lines are ignored.
 
-=head1 SEE ALSO
-
-L<Config::JFDI>
+This module requires Perl 5.10 but it could easily be modified to also run in
+more ancient versions of Perl.
 
 =head1 AUTHOR
 
